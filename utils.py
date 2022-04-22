53cf87e2adc273503fd548d16e05b3d0916098f0
minaremeli <mincsek@gmail.com>
2022-04-22 13:06:46 +0200

    Fix bug in calculating equivalent feature perturbation

--- utils.py
+++ utils.py
@@ -7,17 +7,19 @@ def equivalent_feature_perturbation(target_node, adj, adj_m, features):
     start = time.time()
     modified_features = features.tolil().copy()
 
-    adj_norm = utils.normalize_adj(adj).power(2)
-    adj_m_norm = utils.normalize_adj(adj_m).power(2)
+    adj_norm = utils.normalize_adj(adj.tocsr())
+    adj_norm = adj_norm.dot(adj_norm)
+    adj_m_norm = utils.normalize_adj(adj_m.tocsr())
+    adj_m_norm = adj_m_norm.dot(adj_m_norm)
 
-    adj_norm_ii_inv = np.power(adj_norm.diagonal()[target_node], -1)
+    adj_norm_ii_inv = np.power(adj_norm[target_node, target_node], -1)
 
     adj_diff = adj_m_norm - adj_norm
 
-    delta = adj_diff @ features
-    modified_features[target_node] += delta[target_node] * adj_norm_ii_inv
+    delta = adj_diff.dot(features) * adj_norm_ii_inv
+    modified_features[target_node] += delta[target_node]
     end = time.time()
     print("Converted structure perturbations to feature perturbations in %f ms" % (end - start))
-    return modified_features
+    return modified_features.tocsr(), delta[target_node]
 
 
